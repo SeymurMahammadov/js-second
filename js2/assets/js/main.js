82676cfb1fea622d80455503dcb5663fd1cc5977
SeymurMahammadov <112489438+SeymurMahammadov@users.noreply.github.com>
2022-11-22 13:55:43 +0100

    First

---
+++ js2/assets/js/main.js
@@ -0,0 +1,59 @@
+// Task1
+
+// function go(){
+//     var text="Sayta daxil ol"
+
+//     if (confirm(text)==true) {
+//         text="xos geldin"
+//         document.getElementById('user').style.backgroundColor='red'
+//     } else {
+//         alert("xos getdin")
+//         location='https://www.google.com/'
+//     }
+//     document.getElementById('user').innerHTML=text;
+// }
+
+
+//Task2
+
+// function promo(){
+//     var age=prompt('Yasini qeyd ele gorek')
+    
+
+//     if (age<18) {
+
+//         text='getdin isine'
+        
+//     } else {
+
+//         text="salam salam"
+        
+//     }
+//     document.getElementById('user').innerHTML=text
+// }
+
+//Task3
+
+// function group(){
+   
+
+//     let z=document.getElementById('input').value;
+//     y=z.slice(-3, -2);
+//     if (y==1) {
+     
+//      document.body.style.backgroundColor='yellow'
+//      text='seher qrupu'
+//     }
+
+//     else if (y==2) {
+//         document.body.style.backgroundColor='red'
+//         text='gunorta qrupu' 
+//     }
+   
+//     else if (y==3) {
+//         document.body.style.backgroundColor='black'
+        
+//       text='axsam qrupu'
+//     }
+//     document.getElementById('user').innerHTML=text
+// }
